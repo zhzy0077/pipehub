2b1c22e227fb25b344bd5cf8c1127db1ba16cc54
Zhiyuan Zheng <zhzy0077@hotmail.com>
2022-02-14 14:02:15 +0800

    Update User.tsx

--- web/src/User.tsx
+++ web/src/User.tsx
@@ -3,7 +3,7 @@ import { Label, TextField, PrimaryButton, Separator, Text, DefaultButton, Callou
 import { useBoolean } from '@uifabric/react-hooks';
 import Send from './Send';
 
-const backend = process.env.BACKEND_URL ?? "http://localhost:8080";
+const backend = process.env.BACKEND_URL ?? "https://api.pipehub.net";
 
 function User() {
   const [user, setUser] = useState({} as User);
